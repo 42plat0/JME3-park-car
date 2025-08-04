1d5a77f6295f6c8c9d2338b4d92a1718c7dc0509
penguin-wings <gytisnaujasis@icloud.com>
2025-08-04 21:31:38 +0300

    add front wheeel steering
    add half the job for steering

--- src/main/java/com/parkcar/CarControl.java
+++ src/main/java/com/parkcar/CarControl.java
@@ -16,16 +16,19 @@ import com.jme3.math.Vector3f;
 import com.jme3.ui.Picture;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Quad;
-
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 /**
  *
  * @author arpyg
  */
 public class CarControl extends AbstractControl {
-    
+
+    private final float STEER_ANGLE = FastMath.PI / 8;
+
     public boolean up, down, left, right;
-    
     private final float speed = 5f;
+    
     private int width, height;
     
     public CarControl(int width, int height){
@@ -36,14 +39,44 @@ public class CarControl extends AbstractControl {
     
     @Override
     protected void controlUpdate(float timePerFrame){
-        Vector3f cords = new Vector3f(0, 0, 0);
-        Spatial car = spatial;
+        Vector3f cords = new Vector3f(0, 0, 230);
+        Node car = (Node) spatial;
 
-        System.out.println(car.getClass());
+        Geometry frontWheel = (Geometry) car.getChild("frontWheel");
         // Need: car position centre
         // Wheel baseline and two wheels at the ends
-        // Rotate rear one
+        // Rotate rear one -- CURRENT how to move rear wheel?
         // Move in the direction it's pointing
+
+        
+        Quaternion currentWheelRotation = frontWheel.getLocalRotation();
+        Vector3f axis = new Vector3f();
+        float angle = currentWheelRotation.toAngleAxis(axis) * 0.5f * 0.5f;
+        
+        Quaternion carRotation = car.getLocalRotation();
+        
+        Vector3f carRotated = carRotation.mult(Vector3f.UNIT_Z);
+        Vector3f rotated = currentWheelRotation.mult(Vector3f.UNIT_Z);
+        
+        System.out.println(angle);
+
+//                System.out.println(carRotation.mult(currentWheelRotation));
+//                System.out.println(carRotation.add(currentWheelRotation));
+
+        // Go with car in direction pointe dby wheel
+        // https://hub.jmonkeyengine.org/t/solved-rotating-vector3f-by-angle/36168/7
+        Quaternion newQ = new Quaternion();
+        
+        if (right){
+            newQ.fromAngleAxis(STEER_ANGLE, new Vector3f(0, 0, -1));
+        } else if (left){
+            newQ.fromAngleAxis(STEER_ANGLE, new Vector3f(0, 0, 1));
+        } else{
+            newQ.fromAngleAxis(STEER_ANGLE, new Vector3f(0, 0, 0));
+        }
+        frontWheel.setLocalRotation(newQ);
+//        Vector3f newWheelPos = q.mult(currentWheelPosition);
+//        frontWheel.setLocalTranslation(newWheelPos);
 
         if (up){
             cords.setY(speed);
@@ -58,5 +91,10 @@ public class CarControl extends AbstractControl {
     @Override
     protected void controlRender(RenderManager rm, ViewPort vp){
         
+    }
+    
+    
+    public float degToRad(float deg){
+        return deg * FastMath.DEG_TO_RAD;
     }
 }
--- src/main/java/com/parkcar/ParkCar.java
+++ src/main/java/com/parkcar/ParkCar.java
@@ -18,19 +18,19 @@ import com.jme3.input.KeyInput;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Quad;
 
-
 import com.parkcar.CarControl;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
  * Move your Logic into AppStates or Controls
+ *
  * @author normenhansen
  */
 public class ParkCar extends SimpleApplication implements ActionListener {
 
     private Picture car;
     private Node carNode;
-    
+
     public static void main(String[] args) {
         ParkCar app = new ParkCar();
         app.setShowSettings(false);
@@ -38,72 +38,86 @@ public class ParkCar extends SimpleApplication implements ActionListener {
     }
 
     @Override
-    public void simpleInitApp() {        
+    public void simpleInitApp() {
         // Camera for 2d
         cam.setParallelProjection(true);
-        cam.setLocation(new Vector3f(0,0,0.5f));
+        cam.setLocation(new Vector3f(0, 0, 0.5f));
         getFlyByCamera().setEnabled(false);
-        
-        
+
         car = new Picture("Car picture");
         car.setImage(assetManager, "Textures/mainCar.png", true);
-        car.setWidth(settings.getWidth()/4);
-        car.setHeight(settings.getHeight()/2);
+        car.setWidth(settings.getWidth() / 4);
+        car.setHeight(settings.getHeight() / 2);
         car.setPosition(200, 200);
-        
+
         carNode = new Node();
-        Quad frontWheel = new Quad(50f, 30f);
-        Geometry fGeomWheel = new Geometry("front wheel", frontWheel);
-        Material fMatWheel = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        fMatWheel.setColor("Color", ColorRGBA.Blue);
-        fGeomWheel.setMaterial(fMatWheel);
-        fGeomWheel.setLocalTranslation(250, 220, 0);
-        
-        Quad backWheel = new Quad(50f, 30f);
-        Geometry bGeomWheel = new Geometry("back wheel", backWheel);
-        Material bMatWheel = fMatWheel.clone();
-        bGeomWheel.setMaterial(bMatWheel);
-        bGeomWheel.setLocalTranslation(250, 340, 0);
-        
-        
+
         carNode.addControl(new CarControl(settings.getWidth(), settings.getHeight()));
         registerInput();
 
         carNode.attachChild(car);
-        carNode.attachChild(fGeomWheel);
-        carNode.attachChild(bGeomWheel);
+        addWheels(carNode);
 
         guiNode.attachChild(carNode);
     }
-    
-    public void registerInput(){
+
+    public void registerInput() {
         String up = "up", down = "down", left = "left", right = "right";
-        
+
         inputManager.addMapping(up, new KeyTrigger(KeyInput.KEY_W));
         inputManager.addMapping(up, new KeyTrigger(KeyInput.KEY_UP));
-        
+
         inputManager.addMapping(right, new KeyTrigger(KeyInput.KEY_D));
         inputManager.addMapping(right, new KeyTrigger(KeyInput.KEY_RIGHT));
 
         inputManager.addMapping(left, new KeyTrigger(KeyInput.KEY_A));
         inputManager.addMapping(left, new KeyTrigger(KeyInput.KEY_LEFT));
 
-        
         inputManager.addMapping(down, new KeyTrigger(KeyInput.KEY_S));
         inputManager.addMapping(down, new KeyTrigger(KeyInput.KEY_DOWN));
 
         inputManager.addListener(this, up, down, left, right);
     }
-        
-    @Override
-    public void onAction(String name, boolean isPressed, float tpf){
-        CarControl carControl = carNode.getControl(CarControl.class);
-        if ("up".equals(name)){
-            carControl.up = isPressed;
-        }
-        else if ("down".equals(name)){
-            carControl.down = isPressed;
 
+    public void addWheels(Node carNode) {
+        int wheelBase = 140;
+
+        float wheelWidth = 20f;
+        float wheelHeight = 30f;
+
+        Quad frontWheel = new Quad(wheelWidth, wheelHeight);
+        float frontWheelY = 360f; // TODO fix based on car
+        Geometry fGeomWheel = new Geometry("frontWheel", frontWheel);
+        Material fMatWheel = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        fMatWheel.setColor("Color", ColorRGBA.Blue);
+        fGeomWheel.setMaterial(fMatWheel);
+        fGeomWheel.setLocalTranslation(270, frontWheelY, 0);
+
+        Quad backWheel = new Quad(wheelWidth, wheelHeight);
+        Geometry bGeomWheel = new Geometry("backWheel", backWheel);
+        Material bMatWheel = fMatWheel.clone();
+        bGeomWheel.setMaterial(bMatWheel);
+        bGeomWheel.setLocalTranslation(270, frontWheelY - wheelBase, 0);
+
+        carNode.attachChild(fGeomWheel);
+        carNode.attachChild(bGeomWheel);
+
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        CarControl carControl = carNode.getControl(CarControl.class);
+        if ("left".equals(name)) {
+            carControl.left = isPressed;
+        } else if ("right".equals(name)) {
+            carControl.right = isPressed;
+        }
+
+        if ("up".equals(name)) {
+            carControl.up = isPressed;
+
+        } else if ("down".equals(name)) {
+            carControl.down = isPressed;
         }
     }
 
@@ -117,4 +131,3 @@ public class ParkCar extends SimpleApplication implements ActionListener {
         //TODO: add render code
     }
 }
-
