45324388126f51ebc363c937f27ed5bcedb7819d
penguin-wings <gytisnaujasis@icloud.com>
2025-08-17 11:08:00 +0300

    Complete car driving mechanism (yet to complete wheel rotations in pointing direction)

--- src/main/java/com/parkcar/CarControl.java
+++ src/main/java/com/parkcar/CarControl.java
@@ -20,17 +20,15 @@ import com.jme3.scene.control.AbstractControl;
 public class CarControl extends AbstractControl {
 
     private final float STEER_ANGLE = FastMath.PI / 8;
+    private final float MAX_SPEED = 300f;
+    private float accel = 8f;
 
     public boolean up, down, left, right;
     private float carHeading = 0f;
-    private float accel = 8f;
     private float steerAngle = 0f; // current front wheel angle (radians)
+    private float currAngle = 0f;
+    private float velocity = 0f;      // current velocity in px/sec
 
-        private float maxSpeed = 300f;    // px/sec^2
-        private float wheelBase = 140f;
-        private float velocity = 0f;      // current velocity in px/sec
-        private float theta = 0f;
-            private Vector3f position = new Vector3f();
     private int width, height;
     
     public CarControl(int width, int height){
@@ -48,36 +46,51 @@ public class CarControl extends AbstractControl {
         handleSteerInput(tpf);
         handleAccelerationInput();
         
-        Vector3f fwt = frontWheel.getWorldTranslation();
-        Vector3f bwt = backWheel.getWorldTranslation();
+        Vector3f carLocation = car.getLocalTranslation();
         
-        carHeading = FastMath.atan2(fwt.getY() - bwt.getY(), fwt.getX() - bwt.getX());
-        
-        Vector3f bwDir = new Vector3f(FastMath.cos(carHeading), FastMath.sin(carHeading), 0);
+        // Deal with front wheel location updates on input
+        Vector3f fWheelLocation = carLocation.add(new Vector3f(FastMath.cos(carHeading), FastMath.sin(carHeading), 0).mult(140f / 2));
+        Vector3f fwt = frontWheel.getLocalTranslation();
         Vector3f fwDir = new Vector3f(FastMath.cos(carHeading + steerAngle), FastMath.sin(carHeading + steerAngle), 0);
-
-        Vector3f bwNewPos = bwt.add(bwDir.mult(velocity * tpf));
-        Vector3f fwNewPos = fwt.add(fwDir.mult(velocity * tpf));
-        
-        backWheel.setLocalTranslation(bwNewPos);
+        // Calculate new position based on calculated instead of retrieved location
+        // to prevent stacking wheels on top and making the whole thing crumble to horrible dust
+        Vector3f fwNewPos = fWheelLocation.add(fwDir.mult(velocity * tpf));
         frontWheel.setLocalTranslation(fwNewPos);
         
+        // Deal with back wheel location updates on input
+        Vector3f bWheelLocation = carLocation.subtract(new Vector3f(FastMath.cos(carHeading), FastMath.sin(carHeading), 0).mult(140f / 2));
+        Vector3f bwt = backWheel.getLocalTranslation();
+        Vector3f bwDir = new Vector3f(FastMath.cos(carHeading), FastMath.sin(carHeading), 0);
+        // Calculate new position based on calculated instead of retrieved location 
+        // to prevent stacking wheels on top and making the whole thing crumble to horrible dust
+        Vector3f bwNewPos = bWheelLocation.add(bwDir.mult(velocity * tpf));
+        backWheel.setLocalTranslation(bwNewPos);
         
-        frontWheel.setLocalRotation(new Quaternion().fromAngleAxis(steerAngle, Vector3f.UNIT_Z));
+        // Update car location
+        car.setLocalTranslation(bwNewPos.add(fwNewPos).divide(2));
+        carHeading = FastMath.atan2(fwt.getY() - bwt.getY(), fwt.getX() - bwt.getX());
 
+       
+        Quaternion fwCurrRot = frontWheel.getLocalRotation();
+        Quaternion ROTATION_ANGLE = new Quaternion().fromAngleAxis(currAngle, Vector3f.UNIT_Z);
+        
+        frontWheel.setLocalRotation(fwCurrRot);
    
     }
     
     private void handleSteerInput(float tpf){
+        float ANGLE = tpf * 2f;
+        
         if (right){
-            steerAngle -= tpf * 2f;
+            steerAngle -= ANGLE;
         } else if (left){
-            steerAngle += tpf * 2f;
+            steerAngle += ANGLE;
         } else{
             steerAngle = FastMath.interpolateLinear(0.2f, steerAngle, 0);
         }
         steerAngle = FastMath.clamp(steerAngle, -STEER_ANGLE, STEER_ANGLE);
-
+        
+        currAngle += steerAngle;
     }
     
     private void handleAccelerationInput(){
@@ -87,9 +100,9 @@ public class CarControl extends AbstractControl {
         else if (down){
             velocity -= accel;
         } else{
-            velocity = 0;
+            velocity = FastMath.interpolateLinear(0.5f, velocity, 0);
         }
-        velocity = FastMath.clamp(velocity, -maxSpeed, maxSpeed);
+        velocity = FastMath.clamp(velocity, -MAX_SPEED, MAX_SPEED);
     }
 
     @Override
--- src/main/java/com/parkcar/ParkCar.java
+++ src/main/java/com/parkcar/ParkCar.java
@@ -6,11 +6,13 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Quad;
+import com.jme3.system.AppSettings;
 import com.jme3.ui.Picture;
 
 /**
@@ -23,10 +25,20 @@ public class ParkCar extends SimpleApplication implements ActionListener {
 
     private Picture car;
     private Node carNode;
+    private Vector3f carLocation = new Vector3f(150, 10, 0);
+    private float carHeading = 0f;
 
     public static void main(String[] args) {
         ParkCar app = new ParkCar();
+  
+        AppSettings defaultSetting = new AppSettings(true);
+        defaultSetting.setTitle("ParkCar");
+        defaultSetting.setHeight(950);
+        defaultSetting.setWidth(1600);
+        defaultSetting.setFrequency(60);
+
         app.setShowSettings(false);
+        app.setSettings(defaultSetting);
         app.start();
     }
 
@@ -36,7 +48,9 @@ public class ParkCar extends SimpleApplication implements ActionListener {
         cam.setParallelProjection(true);
         cam.setLocation(new Vector3f(0, 0, 0.5f));
         getFlyByCamera().setEnabled(false);
-
+      
+        settings.setWidth(5000);
+        settings.setHeight(5000);
 //        car = new Picture("car");
 //        car.setImage(assetManager, "Textures/mainCar.png", true);
 //        car.setWidth(settings.getWidth() / 4);
@@ -49,6 +63,7 @@ public class ParkCar extends SimpleApplication implements ActionListener {
 
 //        carNode.attachChild(car);
         addWheels(carNode);
+        carNode.setLocalTranslation(carLocation);
 
         guiNode.attachChild(carNode);
     }
@@ -72,13 +87,16 @@ public class ParkCar extends SimpleApplication implements ActionListener {
     }
 
     public void addWheels(Node carNode) {
-        int wheelBase = 140;
+    
+        float wheelBase = 140f;
 
         float wheelWidth = 20f;
         float wheelHeight = 30f;
+        
+        Vector3f fWheelLocation = carLocation.add(new Vector3f(FastMath.cos(carHeading), FastMath.sin(carHeading), 0).mult(wheelBase / 2));
+        Vector3f bWheelLocation = carLocation.subtract(new Vector3f(FastMath.cos(carHeading), FastMath.sin(carHeading), 0).mult(wheelBase / 2));
 
         Quad frontWheel = new Quad(wheelWidth, wheelHeight);
-        float frontWheelY = 360f; // TODO fix based on car
         Geometry fGeomWheel = new Geometry("frontWheel", frontWheel);
         Material fMatWheel = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         fMatWheel.setColor("Color", ColorRGBA.Blue);
@@ -87,19 +105,16 @@ public class ParkCar extends SimpleApplication implements ActionListener {
 
         Node pivotNode = new Node("fWheelPivot");
         pivotNode.attachChild(fGeomWheel);
-        fGeomWheel.setLocalTranslation(-wheelWidth/2f, -wheelHeight/2f, 0);
-        pivotNode.setLocalTranslation(270, frontWheelY, 0);
-        
+        pivotNode.setLocalTranslation(fWheelLocation);
         
         Quad backWheel = new Quad(wheelWidth, wheelHeight);
         Geometry bGeomWheel = new Geometry("backWheel", backWheel);
         Material bMatWheel = fMatWheel.clone();
         bGeomWheel.setMaterial(bMatWheel);
-        bGeomWheel.setLocalTranslation(260, frontWheelY - wheelBase, 0);
+        bGeomWheel.setLocalTranslation(bWheelLocation);
         
         carNode.attachChild(pivotNode);
         carNode.attachChild(bGeomWheel);
-
     }
 
     @Override
